5ed6e6b12f240627be84ec50bd8d3ad793d8e36f
Eyal Eisenmann <eyaleisenmannee@gmail.com>
2025-12-02 17:53:57 +0200

    Remove unnecessary message from assertEquals

--- EncryptionTester.java
+++ EncryptionTester.java
@@ -5,10 +5,9 @@ public class EncryptionTest {
 
     @Test
     void testSenLengthOneWord() {
-        // The main method preprocesses the input string before calling SenLength
         String input = "Hello"; 
         int expected = 1;
         int actual = Encryption.SenLength(input);
-        assertEquals(expected, actual, "Should return length of 1");
+        assertEquals(expected, actual);
     }
 }
